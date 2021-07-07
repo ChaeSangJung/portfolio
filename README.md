cde765eeaa211df1cf8866f304b5b7e31af22a35
CheSangJung <shtingstar@hanmail.net>
2021-07-07 11:01:25 +0900

    first setting

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## Portfolio react
+- npm run deploy
---
+++ src/Components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import GlobalStyles from "Components/GlobalStyles";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyles />
+        {/* Header */}
+        <div>
+          xxx
+        </div>
+        {/* Footer */}
+      </>
+    );
+  }
+}
+
+export default App;
