e0c0a0adb04fbcefcaee7c4f85a83ac2fbe5be48
CheSangJung <shtingstar@hanmail.net>
2021-07-07 18:01:10 +0900

    texting

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,14 +4,25 @@ import { contentDb } from "DB/generateDb";
 
 const Main = () => {
     return (
-        <ul>
-            {contentDb.map((db)=>(
-                <ListContainer 
-                    key={db.id}
-                    db={db}
-                />
-            ))}
-        </ul>
+        <>
+            <div>
+                <strong>2020년 이후</strong>
+            </div>
+            <div>
+                <strong>Nomad Coder Challenges</strong>
+                <ul>
+                    {contentDb.map((db)=>(
+                        <ListContainer 
+                            key={db.id}
+                            db={db}
+                        />
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <strong>2020년 이전</strong>
+            </div>
+        </>
     )
 }
 
--- src/DB/generateDb.js
+++ src/DB/generateDb.js
@@ -51,17 +51,75 @@ export const contentDb = [
         ],
         "times" : 4
     },
-    // {
-    //     "id" : uuidv4(),
-    //     "name" : ,
-    //     "title" : ,
-    //     "url" : ,
-    //     "etc" : ,
-    //     "skill" : [
-    //         {
-    //             "id" : uuidv4(),
-    //             "name" : ""
-    //         }
-    //     ]
-    // }
+]
+
+export const forwardDb = () => [
+    {
+        "id" : uuidv4(),
+        "title" : "유튜브 클론 6주 완성반",
+        "skill" : [
+            {
+                "id" : uuidv4(),
+                "name" : "nodeJs",
+            },
+            {
+                "id" : uuidv4(),
+                "name" : "es6",
+            },
+            {
+                "id" : uuidv4(),
+                "name" : "pug",
+            },
+            {
+                "id" : uuidv4(),
+                "name" : "Mongo",
+            }
+        ]
+    },
+    {
+        "id" : uuidv4(),
+        "title" : "인스타그램 클론 6주 완성반",
+        "skill" : [
+            {
+                "id" : uuidv4(),
+                "name" : "nodeJs",
+            },
+            {
+                "id" : uuidv4(),
+                "name" : "GraphQL",
+            },
+            {
+                "id" : uuidv4(),
+                "name" : "react",
+            },
+            
+            {
+                "id" : uuidv4(),
+                "name" : "iOS & Android",
+            }
+        ]
+    },
+    {
+        "id" : uuidv4(),
+        "title" : "우버 이츠 클론 6주 완성반",
+        "skill" : [
+            {
+                "id" : uuidv4(),
+                "name" : "Typescript",
+            },
+            {
+                "id" : uuidv4(),
+                "name" : "Nest",
+            },
+            {
+                "id" : uuidv4(),
+                "name" : "react",
+            },
+            
+            {
+                "id" : uuidv4(),
+                "name" : "Jest",
+            }
+        ]
+    }
 ]
